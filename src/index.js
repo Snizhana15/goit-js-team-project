98487cc35df656b7af29bb1ecf1a5224a50639cf
bragynator <92689909+bragynator@users.noreply.github.com>
2022-06-15 22:49:08 +0300

    add render populat movies and movies navigation html/scss

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { renderPopularMovies } from './js/render-popular-movies';
+
+/*
+Пока что, чтобы посмотреть работает ли пагинация достаточно в функцию renderPopularMovies передать номер страницы в качестве параметра, например renderPopularMovies(3)
+*/
+
+renderPopularMovies();
