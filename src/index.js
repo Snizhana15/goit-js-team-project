d78c08caffc8af1cee656e244eea944daf3134e0
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-06-17 16:02:29 +0300

    made opening modal-film
    Довелося трішки підкорегувати  modal-film.html і modal-film.scss
    
    Все працює, але деякі моменти ще підправлю. Змержіть, щоб всі могли користуватися модалкою для своїх задач.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import { renderPopularMovies } from './js/render-popular-movies';
 Пока что, чтобы посмотреть работает ли пагинация достаточно в функцию renderPopularMovies передать номер страницы в качестве параметра, например renderPopularMovies(3)
 */
 
-renderPopularMovies();
+renderPopularMovies(1);
+
+/*
+Імпорт всього файлу modal-film.js
+*/
+
+import * as things from './js/modal-film';
