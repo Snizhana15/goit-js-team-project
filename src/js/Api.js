d87a406716478b11c11a5fefbafe800d11e43666
mikegurak <92752760+mikegurak@users.noreply.github.com>
2022-06-20 16:08:14 +0300

    Input Search Query Render

--- src/js/Api.js
+++ src/js/Api.js
@@ -18,22 +18,22 @@ export function getPopularMovies(pageNumber = 1) {
 }
 
 /*
-Функцию getMoviesBySearchQuery(searchQuery) используем для запроса при рендере карточек при поиске по ключевому слову.
+Функцию getMoviesBySearchQuery(searchQuery, pageNumber = 1) используем для запроса при рендере карточек при поиске по ключевому слову.
 Параметр searchQuery должен быть строкой из input-а поиска.
 */
 
-export function getMoviesBySearchQuery(searchQuery, pageNumber = 1) {
-  return fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${searchQuery}&language=uk&page=${pageNumber}&include_adult=false`
-  )
-    .then(response => response.json())
-    .then(data => {
-      if (!(data.success ?? true)) {
-        throw new Error(data.status_message);
-      }
-      return data;
-    });
-}
+// export function getSearchQuery(pageNumber = 1) {
+//   return fetch(
+//     `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=superman&language=uk&page=${pageNumber}&include_adult=false`
+//   )
+//     .then(response => response.json())
+//     .then(data => {
+//       if (!(data.success ?? true)) {
+//         throw new Error(data.status_message);
+//       }
+//       return data;
+//     });
+// }
 
 /*
 Функцию getMovieById(id) используем для запроса при отображении полной информации о фильме (модалка при клике на карточку).
@@ -70,4 +70,134 @@ export function getGenreList() {
       }
       return data;
     });
+}
+
+// Render Search Query =======================================================================================
+
+// import { getSearchQuery, getGenreList } from './Api';
+import { changeMoviesPage } from './change-movies-page';
+
+const cardSet = document.querySelector('.card-set');
+const searchInput = document.querySelector('.header__form-input');
+
+searchInput.addEventListener('input', onSearch);
+
+let searchQuery = '';
+
+function getSearchQuery(searchQuery, pageNumber = 1) {
+  const url = 'https://api.themoviedb.org/3';
+  return fetch(`${url}/search/movie?api_key=${KEY}&query=${searchQuery}&language=uk&page=${pageNumber}&include_adult=false`)
+    .then(response => response.json())
+    .then(data => {
+      if (!(data.success ?? true)) {
+        throw new Error(data.status_message);
+      }
+      return data;
+    });
+}
+
+function onSearch(event) {
+  event.preventDefault();
+  searchQuery = event.target.value;
+
+  getSearchQuery();
+  downloadSearchQuery();
+}
+
+async function getGenres() {
+  try {
+    const { genres } = await getGenreList();
+    return genres;
+  } catch (error) {
+    alert(error);
+  }
+}
+
+async function getDataAboutSearchQuery(pageNumber) {
+  try {
+    const { results: movies, total_pages: totalPages } = await getSearchQuery(searchQuery, pageNumber);
+    return { movies, totalPages };
+  } catch (error) {
+    alert(error);
+  }
+}
+
+async function renderSearchQuery(pageNumber) {
+  const genres = await getGenres();
+  const { movies, totalPages } = await getDataAboutSearchQuery(pageNumber);
+
+  const cardsMarkup = movies
+    .map(movie => renderMovieCard(movie, genres))
+    .join('');
+
+  cardSet.innerHTML = '';
+  cardSet.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+  return totalPages;
+}
+
+function getGenresById(idList, genres) {
+  return idList.map(id => genres.find(genre => genre.id === id).name);
+}
+
+function getGenresMarkup(genres) {
+  let genresMarkup = '';
+
+  switch (genres.length) {
+    case 1:
+      genresMarkup = `<li class="card-set__genre-movie">${genres[0]}</li>`;
+      break;
+
+    case 2:
+      genresMarkup = `<li class="card-set__genre-movie">${genres[0]},&nbsp</li>
+                      <li class="card-set__genre-movie">${genres[1]}</li>`;
+      break;
+
+    case 3:
+      genresMarkup = `<li class="card-set__genre-movie">${genres[0]},&nbsp</li>
+                      <li class="card-set__genre-movie">${genres[1]},&nbsp</li>
+                      <li class="card-set__genre-movie">${genres[2]}</li>`;
+      break;
+
+    default:
+      genresMarkup = `<li class="card-set__genre-movie">${genres[0]},&nbsp</li>
+                      <li class="card-set__genre-movie">${genres[1]},&nbsp</li>
+                      <li class="card-set__genre-movie">Інші</li>`;
+  }
+
+  return genresMarkup;
+}
+
+function renderMovieCard(movie, genres) {
+  const { genre_ids, original_title, id, release_date, title, poster_path } =
+    movie;
+
+  const alphabetGenres = getGenresById(genre_ids, genres);
+  const genresMarkup = getGenresMarkup(alphabetGenres);
+  const productionYear = new Date(release_date).getFullYear().toString();
+
+  return `
+        <li class="card-set__item" data-id="${id}">
+            <div class="card-set__box-img">
+            <img
+                loading="lazy"
+                src="https://image.tmdb.org/t/p/original${poster_path}"
+                alt="${original_title}"
+                class="card-set__img"
+                
+            />
+            </div>
+            <h3 class="card-set__title">${title}</h3>
+            <div class="card-set__description">
+            <ul class="card-set__genre">
+                ${genresMarkup}
+            </ul>
+            <span class="card-set__genre-movie">&nbsp| ${productionYear}</span>
+            </div>
+        </li>`;
+}
+    
+async function downloadSearchQuery() {
+  const totalPages = await renderSearchQuery();
+  changeMoviesPage(totalPages > 50 ? 50 : totalPages, renderSearchQuery);
 }
