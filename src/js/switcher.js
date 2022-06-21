8137f73a870f3beab3854162bcc03e9d5a146b2e
YurijKovbasyuk <93015127+YurijKovbasyuk@users.noreply.github.com>
2022-06-21 20:17:09 +0300

    js work

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,21 +1,28 @@
 const buttonTheme = document.querySelector('.switch');
+const body = document.querySelector('#body');
 
 buttonTheme.addEventListener('click', changeTheme);
+
 function changeTheme() {
-  if (buttonTheme.classList.contains('lightTheme')) {
-    buttonTheme.classList.remove('lightTheme');
-    buttonTheme.classList.add('darkTheme');
+  if (body.classList.contains('lightTheme')) {
+    body.classList.remove('lightTheme');
+    body.classList.add('darkTheme');
     localStorage.setItem('theme', 'darkTheme');
   } else {
-    buttonTheme.classList.remove('darkTheme');
-    buttonTheme.classList.add('lightTheme');
+    body.classList.remove('darkTheme');
+    body.classList.add('lightTheme');
     localStorage.setItem('theme', 'lightTheme');
   }
 }
 
 function applyTheme(themeName) {
-  buttonTheme.classList.add(themeName);
+  body.classList.add(themeName);
   localStorage.setItem('theme', themeName);
+  if (themeName === 'lightTheme') {
+    buttonTheme.checked = true;
+  } else {
+    buttonTheme.checked = false;
+  }
 }
 let activeTheme = localStorage.getItem('theme');
 console.log(activeTheme);
