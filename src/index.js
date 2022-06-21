d8cb2b7d16b6ce6143ca7073698f4639dc00b013
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-06-21 22:49:25 +0300

    add import other-genre

--- src/index.js
+++ src/index.js
@@ -17,3 +17,5 @@ import * as things from './js/modal-film';
 import * as elem from './js/modal-students';
 import * as elements from './js/localStorage';
 import * as things from './js/change-button-condition';
+
+import * as elements from './js/other-genre';
